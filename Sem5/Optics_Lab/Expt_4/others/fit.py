496ca711d13378fcc9d960632ecf0187dd8422cd
Aritra Mukhopadhyay <75820716+PeithonKing@users.noreply.github.com>
2023-04-19 11:51:49 +0530

    uv-vis

--- Sem5/Optics_Lab/Expt_4/others/fit.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def least_square_fitting(func, x, y, variables=2, theta=None):
-    import numpy as np
-    if not theta:
-        theta = np.random.rand(variables)
-    y_pred = func(x, *theta)
-    J = np.sum((y - y_pred) ** 2)  # cost function (SSE)
-    slope = [(y-y_pred)]
